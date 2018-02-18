87c0d58d8de78a7f519bef02b1fb6885bccd9d5b
Paula <deadly_dragon13@yahoo.com>
2018-02-18 12:01:21 +0200

    final homework cv online

--- cvonline/cv.js
+++ cvonline/cv.js
@@ -8,12 +8,14 @@
 
 (function () {
     let id = window.location.hash;
-
+    
     if (id) {
+        //show container
         if ($(id).hasClass('disabled')) {
             $(id).siblings('.container').removeClass('active').addClass('disabled');
             $(id).addClass('active').removeClass('disabled');
         }
+        //activate link menu
         if (!$('.link-a[href="' + id + '"]').parent().hasClass('active-btn')) {
             $('.link-a[href="' + id + '"]').parent().siblings('.active-btn').removeClass('active-btn');
             $('.link-a[href="' + id + '"]').parent().addClass('active-btn');
@@ -21,16 +23,16 @@
     }
 
     $('.main-menu .link').click(function (e) {
-        console.log(e, e.target);
-
+//        console.log(e, e.target);
         let id = $(e.target).attr('href') ? $(e.target).attr('href') : $(e.target).parent('a').attr('href');
-        console.log(id, window.location.hash);
+        //activate link menu
         if ($(this).hasClass('active-btn')) {
             $(this).siblings('.active-btn').removeClass('active-btn');
         } else {
             $(this).siblings('.active-btn').removeClass('active-btn');
             $(this).addClass('active-btn');
         }
+        //show container
         if ($(id).hasClass('disabled')) {
             $(id).siblings('.container').removeClass('active').addClass('disabled');
             $(id).addClass('active').removeClass('disabled');
